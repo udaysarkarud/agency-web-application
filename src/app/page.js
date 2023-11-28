f1ac5a580b46227dca255fc1bd923f392e6c8838
udaysarkarud <udayssarkarud@gmail.com>
2023-11-28 16:45:28 +0600

    feat:footer element

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Footer from "@/components/Footer/Footer";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <button className="btn btn-primary">Primary</button>
+      <Footer />
     </>
   );
 }
