4635ee1466bf23cdeb2b921afb6b54033ad3a997
udaysarkarud <udayssarkarud@gmail.com>
2023-11-29 21:15:54 +0600

    fix:change product layout

--- src/components/FeaturedProject/FeaturedProject.jsx
+++ src/components/FeaturedProject/FeaturedProject.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import ComponentsHeading from "../ComponentsHeading/ComponentsHeading";
 import FeaturedProjectCard from "./FeaturedProjectCard";
 
@@ -14,10 +15,30 @@ const FeaturedProject = async () => {
         headingTitle={"Featured Project"}
         headingDes={"We provide the Perfect Solution to your business growth"}
       />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {data.map((fproduct) => {
-          return <FeaturedProjectCard key={fproduct.id} data={fproduct} />;
-        })}
+      <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="rounded overflow-hidden shadow-lg justify-center">
+            <Image
+              src={data[0]["image"]}
+              width={500}
+              height={500}
+              className="w-full h-2/3"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{data[0]["title"]}</div>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                {data.remark}
+              </span>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {data.slice(2).map((fproduct) => {
+              return <FeaturedProjectCard key={fproduct.id} data={fproduct} />;
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/FeaturedProject/FeaturedProjectCard.jsx
+++ src/components/FeaturedProject/FeaturedProjectCard.jsx
@@ -5,13 +5,8 @@ const FeaturedProjectCard = ({ data }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <Image src={data.image} width={200} height={200} className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{data.title}</div>
-      </div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          {data.remark}
-        </span>
+      <div className="px-6 py-1">
+        <div className="font-bold text-sm mb-2">{data.title}</div>
       </div>
     </div>
   );
