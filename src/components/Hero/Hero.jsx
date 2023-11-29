05d896fa849c7f35213c257295f6250a24894875
udaysarkarud <udayssarkarud@gmail.com>
2023-11-29 10:56:47 +0600

    Fix:hero responsiv

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,13 +13,13 @@ const Hero = async () => {
   return (
     <div className="bg-green-300">
       <Navbar />
-      <div className="hero-section py-10 flex items-center justify-around">
-        <div className="w-1/3 space-y-5 ">
+      <div className="hero-section p-10 flex flex-col md:flex-row md:items-center md:justify-between space-y-5 md:space-y-0">
+        <div className="md:w-1/3 space-y-5 ">
           <h1 className="font-extrabold text-5xl">{data.title}</h1>
           <p>{data.description}</p>
           <button className="btn btn-success text-white">Get Started</button>
         </div>
-        <div className="w-1/2 flex gap-2 justify-end">
+        <div className="md:w-1/2 flex gap-2 justify-end">
           <div className="flex flex-col  gap-2">
             <Image
               src={data.image1}
