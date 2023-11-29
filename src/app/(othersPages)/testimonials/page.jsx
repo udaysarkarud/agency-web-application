a360701195060fbe9576c9a565482c534d751d41
udaysarkarud <udayssarkarud@gmail.com>
2023-11-29 19:56:07 +0600

    feat: added testimonial

--- src/app/(othersPages)/testimonials/page.jsx
+++ src/app/(othersPages)/testimonials/page.jsx
@@ -1,10 +1,47 @@
+import ComponentsHeading from "@/components/ComponentsHeading/ComponentsHeading";
+import Image from "next/image";
 import React from "react";
-
-const page = () => {
+async function getData() {
+  const res = await fetch("https://agency.teamrabbil.com/api/TestimonialList");
+  const data = res.json();
+  return data;
+}
+const page = async () => {
+  const data = await getData();
   return (
-    <div>
-      <h1>testimonials poage</h1>
-    </div>
+    <>
+      <ComponentsHeading
+        headingTitle={"Testimonial List"}
+        headingDes={"Better Agency/SEO Solution At Your Fingertips"}
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {data.map((testimonial) => {
+          return (
+            <div
+              key={testimonial.id}
+              className="card w-96 bg-base-100 shadow-xl"
+            >
+              <figure>
+                <Image
+                  src={testimonial.image}
+                  width={100}
+                  height={100}
+                  alt={testimonial.name}
+                  className="rounded-xl shadow-2xl"
+                />
+              </figure>
+              <div className="card-body text-center">
+                <p>{testimonial.msg}</p>
+                <h2 className="text-center font-extrabold text-2xl">
+                  {testimonial.name}
+                </h2>
+                <p>{testimonial.designation}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
