08f4d3048ad93b3a31c7097e1c9c74c8a448b2ab
udaysarkarud <udayssarkarud@gmail.com>
2023-11-30 13:54:30 +0600

    fix: change btn color

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -12,13 +12,13 @@ const Subscribe = () => {
           <p>
             Please drop your email below to get daily update about what we do
           </p>
-          <div className="mt-4">
+          <div className="mt-4 md:border-2  md:w-6/12 md:p-1 md:mx-auto rounded-lg">
             <input
               type="text"
               placeholder="Type here"
-              className="input input-bordered input-accent"
+              className="input input-bordered"
             />
-            <button className="btn btn-primary mt-2 md:mt-0 md:ml-5">
+            <button className="btn bg-orange-600 mt-2 md:mt-0 md:ml-3 text-white">
               Get Started
             </button>
           </div>
